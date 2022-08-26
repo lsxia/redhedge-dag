33cdd26256723eadd2e8de8e3919fff514f1bde2
Nano112 <harrisonbastian.nano@gmail.com>
2022-08-26 17:20:59 +0100

    added pnl-dag

---
+++ dags/pnl_dag.py
@@ -0,0 +1,42 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.http_operator import SimpleHttpOperator
+
+with DAG(
+    dag_id='pnl_dag',
+    description='PNL DAG',
+    schedule_interval='*/1 * * * *',
+    start_date=datetime(2019, 1, 1),
+    catchup=False,
+) as dag:
+    spread_pnl = SimpleHttpOperator(
+        task_id='spread_pnl',
+        http_conn_id='open_faas',
+        endpoint='spread-pnl',
+        method='GET',
+    )
+    cash_pnl = SimpleHttpOperator(
+        task_id='cash_pnl',
+        http_conn_id='open_faas',
+        endpoint='cash-pnl',
+        method='GET',
+    )
+    gross_reduction = SimpleHttpOperator(
+        task_id='gross_reduction',
+        http_conn_id='open_faas',
+        endpoint='gross-reduction',
+        method='GET',
+    )
+    per_book_cash_pnl = SimpleHttpOperator(
+        task_id='per_book_cash_pnl',
+        http_conn_id='open_faas',
+        endpoint='per-book-cash-pnl',
+        method='GET',
+    )
+    delta_pnl = SimpleHttpOperator(
+        task_id='delta_pnl',
+        http_conn_id='open_faas',
+        endpoint='delta-pnl',
+        method='GET',
+    )
+    spread_pnl >> cash_pnl >> [gross_reduction, per_book_cash_pnl] >> delta_pnl
