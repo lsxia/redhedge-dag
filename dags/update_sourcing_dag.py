d7342680cbbe3580ae8ac4903c8f8ca06bb2d810
Nano112 <harrisonbastian.nano@gmail.com>
2022-11-08 16:04:16 +0000

    added start ressourcing

--- dags/update_sourcing_dag.py
+++ dags/update_sourcing_dag.py
@@ -16,16 +16,29 @@ def is_response_ok(response) -> bool:
     
 with DAG(
     dag_id="update_sourcing",
-    description="Updates the start of day sourcing DAG",
-    schedule_interval="*/5 8-17 * * *",
+    description="Updates the start of day sourcing DAG with the latest data ( every 10 minutes)",
+    schedule_interval="*/10 8-17 * * *",
     start_date=datetime(2019, 1, 1),
     catchup=False,
 ) as dag:
-
+    update_positions = SimpleHttpOperator(
+        task_id="start_of_day_import",
+        http_conn_id="open_faas",
+        endpoint="start-of-day-import",
+        method="GET",
+        response_check=is_response_ok,
+    )
     update_intraday = SimpleHttpOperator(
         task_id="intraday_import",
         http_conn_id="open_faas",
         endpoint="intraday-import",
+        method="GET",
+        response_check=is_response_ok,
+    )
+    update_sourced_risk_data = SimpleHttpOperator(
+        task_id="update_sourced_risk_data",
+        http_conn_id="open_faas",
+        endpoint="update-sourced-risk-data",
         method="GET",
         response_check=is_response_ok,
     )
@@ -37,5 +50,8 @@ with DAG(
         response_check=is_response_ok,
     )
     (
-        update_intraday
+        update_positions
+        >> update_intraday
+        >> update_sourced_risk_data
+        >> compute_risk_data
     )
