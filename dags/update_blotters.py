580494f8c63de99dec80a66d4912e869dcf464e1
Nano112 <harrisonbastian.nano@gmail.com>
2022-11-15 16:07:52 +0000

    split dag

--- dags/update_blotters.py
+++ dags/update_blotters.py
@@ -15,28 +15,28 @@ def is_response_ok(response) -> bool:
         return ok
     
 with DAG(
-    dag_id="update_sourcing",
-    description="Updates the start of day sourcing DAG with the latest data ( every hour)",
-    schedule_interval="0 8-17 * * *",
+    dag_id="update_blotters",
+    description="Updates the start of day sourcing DAG with the latest data ( every 10 minutes)",
+    schedule_interval="*/10 8-17 * * *",
     start_date=datetime(2019, 1, 1),
     catchup=False,
 ) as dag:
+    update_positions = SimpleHttpOperator(
+        task_id="start_of_day_import",
+        http_conn_id="open_faas",
+        endpoint="start-of-day-import",
+        method="GET",
+        response_check=is_response_ok,
+    )
+    update_intraday = SimpleHttpOperator(
+        task_id="intraday_import",
+        http_conn_id="open_faas",
+        endpoint="intraday-import",
+        method="GET",
+        response_check=is_response_ok,
+    )
 
-    update_sourced_risk_data = SimpleHttpOperator(
-        task_id="update_sourced_risk_data",
-        http_conn_id="open_faas",
-        endpoint="update-sourced-risk-data",
-        method="GET",
-        response_check=is_response_ok,
-    )
-    compute_risk_data = SimpleHttpOperator(
-        task_id="compute_risk_data",
-        http_conn_id="open_faas",
-        endpoint="compute-risk-data",
-        method="GET",
-        response_check=is_response_ok,
-    )
     (
-        update_sourced_risk_data
-        >> compute_risk_data
+        update_positions
+        >> update_intraday
     )
